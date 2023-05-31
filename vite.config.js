4592f04db5b8e9c03008ede3d540d531f7fbf49c
anzarbk <ansarzk4@gmail.com>
2023-05-31 18:02:25 +0530

    second commit

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 //https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    outDir: 'build', // set the output directory to build
+    outDir: 'dist', // set the output directory to build
   },
   plugins: [react()],
 });
