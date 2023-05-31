9c497fda3767e8fc015e6b156ed20f2291ee357d
anzarbk <ansarzk4@gmail.com>
2023-05-31 17:55:07 +0530

    second commit

--- src/components/User-UI/Banner.jsx
+++ src/components/User-UI/Banner.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Flower from '../../images/Banner2.avif';
+import Flower from '../../images/banner2.avif';
 const Banner = () => {
   return (
     <div className="flex justify-center mt-10  bg-black">
